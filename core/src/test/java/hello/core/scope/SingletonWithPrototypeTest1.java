99f5a25ebc99ccaf1770d59552d3beb2bbf321b1
CloakingGhost <project0858@gmail.com>
2024-01-19 00:26:56 +0900

    Scpoe.Prototype

---
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,98 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        Assertions.assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
+
+        ac.close();
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class, ClientBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+//        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    @RequiredArgsConstructor
+    static class ClientBean {
+        private final PrototypeBean prototypeBean; // 생성 시점에 주입
+
+        //필요한 빈을 대신 찾아주는 대리자의 역할
+        //스프링에 의존적
+//        private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        // 라이브러리 추가 후 사용 가능, 자바 표준
+        private final Provider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); // ObjectProvider
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); // Provider
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("ClientBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("ClientBean.destroy");
+        }
+    }
+
+    @Getter
+    @Setter
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            System.out.println("PrototypeBean.addCount");
+            count++;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("Prototype.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("Prototype.destroy");
+        }
+    }
+}
