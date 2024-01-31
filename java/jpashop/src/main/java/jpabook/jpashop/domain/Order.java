54dba049071a4fa33d33b86aa4f6f8f24c5d2bb3
CloakingGhost <project0858@gmail.com>
2024-01-31 17:45:58 +0900

    Order service, repository and entity business logic

--- java/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ java/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,8 +1,11 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -12,6 +15,7 @@ import java.util.List;
 @Table(name = "orders")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id
@@ -31,10 +35,10 @@ public class Order {
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
-    private LocalDateTime localDateTime; // 주문시간
+    private LocalDateTime orderDate; // 주문시간
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatus; // ORDER, CANCEL
+    private OrderStatus status; // ORDER, CANCEL
 
     //== 연관관계 편의 메서드 ==//
     //== 관리를 하는 쪽에 작성한다 ==//
@@ -44,7 +48,7 @@ public class Order {
         member.getOrders().add(this); // 메서드 하나로 주문목록에 한번에 추가한다
     }
 
-    public void setOrderItem(OrderItem orderItem) {
+    public void addOrderItem(OrderItem orderItem) {
         this.orderItems.add(orderItem);
         orderItem.setOrder(this); // 메서드 하나로 주문정보를 지정한다
     }
@@ -54,8 +58,42 @@ public class Order {
         delivery.setOrder(this); // 메서드 하나로 주문정보를 지정한다
     }
 
+    //==생성 메서드==//
+    /*생성이 단순하지 않기 때문에 따로 만들어준다*/
+    //기본생성자 사용을 막아야한다 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
 
+    //==비즈니스 로직==//
+    /*주문 취소*/
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
 
+    //==조회 로직==//
+    /*전체 주문 가격 조회*/
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 
 
 }
